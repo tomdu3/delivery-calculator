4aa48f264155cac1744134e1859afbafc565ebe0
Tomislav Dukez <tomdu3@ymail.com>
2024-01-26 17:26:50 +0000

    Fix: rush charge default value from 0 to 1 and add comments

--- calculator.py
+++ calculator.py
@@ -1,12 +1,6 @@
 from datetime import datetime, timezone
 
 
-req_input = {
-    "cart_value": 790,
-    "delivery_distance": 2235,
-    "number_of_items": 4,
-    "time": "2024-01-15T13:00:00Z"
-}
 
 MIN_CART_VALUE = 10
 BASIC_DISTANCE_CHARGE = 2
@@ -27,6 +21,9 @@ def sur_charge(value):
 
 
 def charge_distance(distance):
+    '''
+    Calculates delivery charges according to the distance
+    '''
     if distance <= BASIC_DISTANCE:
         return BASIC_DISTANCE_CHARGE
     else:
@@ -38,6 +35,10 @@ def charge_distance(distance):
 
 
 def charge_number_of_items(number_of_items):
+    '''
+    Calculates and returns the charges depending on the number of items:
+    Under minimum number, bulk, ...
+    '''
     total_charge = 0
     if number_of_items > 4:
         total_charge += (number_of_items - 4) * .50
@@ -47,7 +48,12 @@ def charge_number_of_items(number_of_items):
 
 
 def friday_rush(order_date):
-    rush_charge = 0
+    '''
+    Checks if the given day is Friday and if the time slot is between
+    3pm and 7 pm and calculates and returnes the extra charges accordingly
+    '''
+
+    rush_charge = 1
     datetime_object = datetime.fromisoformat(order_date)
     day = datetime_object.isoweekday()
     time = datetime_object.hour
@@ -57,21 +63,46 @@ def friday_rush(order_date):
 
 
 def calculate_bill(data):
+    '''
+    Main function. Calculates and returns the total of all the bill charges.
+    It waves all charges (delivery and other fees) if the value of the order
+    is of 200 or more.
+    It doesn't go over 15 for the sum of delivery and other charges.
+    '''
+
     amount = data['cart_value']
     distance = data['delivery_distance']
     number_of_items = data['number_of_items']
     time = data['time']
+
+    # if the cart value is greater or equal to the minimal free delivery,
+    # there are no extra charges for delivery and other services
     if amount >= MIN_DELIVERY_FREE:
         delivery_fee = 0
     else:
+        # calculates extra charges and delivery costs
         min_amount_charge = sur_charge(amount)
         distance_charge = charge_distance(distance)
         bulk_charge = charge_number_of_items(number_of_items)
+        # total of the fees
         delivery_fee = (min_amount_charge + distance_charge + bulk_charge) *\
             friday_rush(time)
+        # not overpassing the alowed fee amount
         delivery_fee = min(delivery_fee, MAX_DELIVERY_FEE)
 
     return amount + delivery_fee
 
 
-print(calculate_bill(req_input))
+if __name__ == "__main__":
+    '''
+    It runs only if the script is run directly and not imported as a module
+    '''
+
+    # sample input
+    req_input = {
+        "cart_value": 790,
+        "delivery_distance": 2235,
+        "number_of_items": 4,
+        "time": "2024-01-15T13:00:00Z"
+    }
+    print(calculate_bill(req_input))
