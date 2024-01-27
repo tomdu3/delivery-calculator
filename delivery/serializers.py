e61e037d2c395733847643109e7fbb759bdcbd1d
Tomislav Dukez <tomdu3@ymail.com>
2024-01-27 19:01:32 +0000

    Add validation for numeric values- not < 1

--- delivery/serializers.py
+++ delivery/serializers.py
@@ -2,20 +2,6 @@ from rest_framework import serializers
 from .models import Delivery
 
 
-class DeliverySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Delivery
-        fields = (
-            'id',
-            'cart_value',
-            'delivery_distance',
-            'number_of_items',
-            'time',
-            'delivery_fee'
-        )
-
-
 class DeliveryFeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Delivery
@@ -25,3 +11,18 @@ class DeliveryFeeSerializer(serializers.ModelSerializer):
             'number_of_items',
             'time'
         )
+
+    def validate_cart_value(self, value):
+        if value < 1:
+            raise serializers.ValidationError("Cart value must be at least 1")
+        return value
+
+    def validate_delivery_distance(self, value):
+        if value < 1:
+            raise serializers.ValidationError("Delivery distance must be at least 1")
+        return value
+
+    def validate_number_of_items(self, value):
+        if value < 1:
+            raise serializers.ValidationError("Number of items must be at least 1")
+        return value
--- delivery/views.py
+++ delivery/views.py
@@ -1,6 +1,4 @@
-from django.views import View
-from django.shortcuts import render
-from rest_framework import viewsets, generics, status
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Delivery
