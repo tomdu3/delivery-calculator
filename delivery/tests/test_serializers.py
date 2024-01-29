8a6efe8f5d8f6672cd5d3d4633e590406c7bcf86
Tomislav Dukez <tomdu3@ymail.com>
2024-01-29 13:22:52 +0000

    Add 2 other tests for invalid data

--- delivery/tests/test_serializers.py
+++ delivery/tests/test_serializers.py
@@ -27,6 +27,22 @@ class DeliveryFeeSerializerTestCase(TestCase):
         self.assertFalse(serializer.is_valid())
         self.assertIn('cart_value', serializer.errors)
 
+    def test_invalid_delivery_distance_value(self):
+        # Test the serializer with invalid data
+        invalid_data = self.valid_data.copy()  # copy the valid data sample
+        invalid_data['delivery_distance'] = 0  # invalid distance value
+        serializer = DeliveryFeeSerializer(data=invalid_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('delivery_distance', serializer.errors)
+
+    def test_invalid_number_of_items(self):
+        # Test the serializer with invalid data
+        invalid_data = self.valid_data.copy()  # copy the valid data sample
+        invalid_data['number_of_items'] = 0  # invalid number of items
+        serializer = DeliveryFeeSerializer(data=invalid_data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('number_of_items', serializer.errors)
+
     def test_invalid_time_string_format(self):
         # Test the serializer with invalid data
         invalid_data = self.valid_data.copy()  # copy the valid data sample
